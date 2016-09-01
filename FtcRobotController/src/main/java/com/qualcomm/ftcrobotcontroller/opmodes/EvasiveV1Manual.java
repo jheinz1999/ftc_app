4de0c97726ee3342889d1dc664080071dbf98787
Ric Allinson <allinson@yahoo-inc.com>
2016-09-01 15:55:01 -0700

    Added basic code for moving the robot.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EvasiveV1Manual.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EvasiveV1Manual.java
@@ -1,11 +1,23 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by allinson on 9/1/16.
  */
 public class EvasiveV1Manual extends OpMode {
+
+    /*
+     * The left motor controller instance.
+     */
+    protected DcMotor leftDriveMotor;
+
+    /*
+     * The right motor controller instance.
+     */
+    protected DcMotor rightDriveMotor;
 
     /**
      * Perform any actions that are necessary when the OpMode is enabled.
@@ -13,7 +25,19 @@ public class EvasiveV1Manual extends OpMode {
      * The system calls this member once when the OpMode is enabled.
      */
     @Override public void init () {
-
+        // Get the left drive motor.
+        try {
+            this.leftDriveMotor = this.hardwareMap.dcMotor.get("leftDriveMotor");
+        } catch (Exception e) {
+            DbgLog.msg (e.getLocalizedMessage());
+        }
+        // Get the right drive motor.
+        try {
+            this.rightDriveMotor = this.hardwareMap.dcMotor.get("rightDriveMotor");
+            this.rightDriveMotor.setDirection (DcMotor.Direction.REVERSE);
+        } catch (Exception e) {
+            DbgLog.msg (e.getLocalizedMessage());
+        }
     }
 
     /**
@@ -31,7 +55,8 @@ public class EvasiveV1Manual extends OpMode {
      * The system calls this member repeatedly while the OpMode is running.
      */
     @Override public void loop () {
-
+        this.leftDriveMotor.setPower(gamepad1.left_stick_y);
+        this.rightDriveMotor.setPower(gamepad1.right_stick_y);
     }
 
     /**
