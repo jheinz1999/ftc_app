f1b9e1a267ef4c85c2463dd59d587ee21097e82f
Ric Allinson <allinson@yahoo-inc.com>
2016-09-01 13:06:30 -0700

    Created Classes for Evasive Robot

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EvasiveV1Auto.java
@@ -0,0 +1,45 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by allinson on 9/1/16.
+ */
+public class EvasiveV1Auto extends OpMode {
+
+    /**
+     * Perform any actions that are necessary when the OpMode is enabled.
+     *
+     * The system calls this member once when the OpMode is enabled.
+     */
+    @Override public void init () {
+
+    }
+
+    /**
+     * Perform any actions that are necessary when the OpMode is enabled.
+     *
+     * The system calls this member once when the OpMode is enabled.
+     */
+    @Override public void start () {
+
+    }
+
+    /**
+     * Perform any actions that are necessary while the OpMode is running.
+     *
+     * The system calls this member repeatedly while the OpMode is running.
+     */
+    @Override public void loop () {
+
+    }
+
+    /**
+     * Perform any actions that are necessary when the OpMode is disabled.
+     *
+     * The system calls this member once when the OpMode is disabled.
+     */
+    @Override public void stop () {
+
+    }
+}
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -57,18 +57,22 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("NullOp", NullOp.class);
 
+    // Evasive Operation Modes.
+    manager.register("EvasiveV1Auto", EvasiveV1Auto.class);
+    manager.register("EvasiveV1Manual", EvasiveV1Manual.class);
+
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register("K9Line", K9Line.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
+    //manager.register("K9Line", K9Line.class);
+    //manager.register ("PushBotAuto", PushBotAuto.class);
+    //manager.register ("PushBotManual", PushBotManual.class);
 
 
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
-    manager.register("MR Gyro Test", MRGyroTest.class);
+    //manager.register("MR Gyro Test", MRGyroTest.class);
 
     //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
